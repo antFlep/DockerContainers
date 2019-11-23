e2055000e74b8e7a5a9002f49b0106ead693c387
Filipe <fa5299@hotmail.com>
2019-11-23 21:05:14 +0100

    fixed getradical.sh: double quotes instead of single quotes with sed
    If we use single quotes with the sed command our bash variables will not be resolved.

--- Radicale/getradicale.sh
+++ Radicale/getradicale.sh
@@ -15,6 +15,6 @@ htpasswd -B -c /resources/users username
 curl -JLO https://raw.githubusercontent.com/Kozea/Radicale/master/Dockerfile
 
 # use change radicale version in dockerfile
-sed -i 's/^ARG VERSION=master/ARG VERSION=$version/g' Dockerfile
+sed -i "s/^ARG VERSION=master/ARG VERSION=$version/g" Dockerfile
 # copy the files inside the resources directory into the /etc/radicale directory
-sed -i '0,/^VOLUME/s/^VOLUME/\nCOPY .\/ressources\/ \/etc\/radicale\n\n&/' Dockerfile
+sed -i "0,/^VOLUME/s/^VOLUME/\nCOPY .\/ressources\/ \/etc\/radicale\n\n&/" Dockerfile
